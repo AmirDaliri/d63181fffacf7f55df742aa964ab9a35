a5b21ef262f3414dc9521201d2fb27950038dc49
emir daliri <amir.dalirii69@gmail.com>
2020-12-22 23:51:00 +0300

    initial

---
+++ d63181fffacf7f55df742aa964ab9a35/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  d63181fffacf7f55df742aa964ab9a35
+//
+//  Created by amir on 22.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
