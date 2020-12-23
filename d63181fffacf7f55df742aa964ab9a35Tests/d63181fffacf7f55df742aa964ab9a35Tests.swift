2fd31f14179e4fb2867e1fa90bcb1f65cfe1c9ad
emir daliri <amir.dalirii69@gmail.com>
2020-12-23 21:45:29 +0300

    encoding test

--- d63181fffacf7f55df742aa964ab9a35Tests/d63181fffacf7f55df742aa964ab9a35Tests.swift
+++ d63181fffacf7f55df742aa964ab9a35Tests/d63181fffacf7f55df742aa964ab9a35Tests.swift
@@ -30,4 +30,28 @@ class d63181fffacf7f55df742aa964ab9a35Tests: XCTestCase {
         }
     }
 
+    func testEncoding() {
+        guard let url = URL(string: "https:www.google.com/") else {
+            XCTAssertTrue(false, "Could not instantiate url")
+            return
+        }
+        var urlRequest = URLRequest(url: url)
+        let parameters: Parameters = [
+            "UserID": 1,
+            "Name": "Malcolm",
+            "Email": "malcolm@network.com",
+            "IsCool": true
+        ]
+        do {
+            let encoder = URLParameterEncoder()
+            try encoder.encode(urlRequest: &urlRequest, with: parameters)
+            guard let fullURL = urlRequest.url else {
+                XCTAssertTrue(false, "urlRequest url is nil.")
+                return
+            }
+
+            let expectedURL = "https:www.google.com/?Name=Malcolm&Email=malcolm%2540network.com&UserID=1&IsCool=true"
+            XCTAssertEqual(fullURL.absoluteString.sorted(), expectedURL.sorted())
+        } catch {}
+    }
 }
