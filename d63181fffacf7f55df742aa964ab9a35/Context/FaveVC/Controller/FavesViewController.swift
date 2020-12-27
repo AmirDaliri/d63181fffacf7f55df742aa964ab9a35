7ca4fa9e99b249e5134f1b09ef5df551acc4571a
emir daliri <amir.dalirii69@gmail.com>
2020-12-27 17:19:11 +0300

    faves vc UI, functionality

--- d63181fffacf7f55df742aa964ab9a35/Context/FaveVC/Controller/FavesViewController.swift
+++ d63181fffacf7f55df742aa964ab9a35/Context/FaveVC/Controller/FavesViewController.swift
@@ -7,9 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
 class FavesViewController: BaseVC {
 
+    // MARK: - IBOutlet
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.register(cellClass: FaveTableViewCell.self)
+            tableView.tableFooterView = UIView()
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+    
+    // MARK: - Properties
+    private var viewModel = FavesViewModel()
+    private var disposal = Disposal()
+    
     // MARK: - Lifecycle Methods
     
     override func viewDidLoad() {
@@ -17,6 +32,102 @@ class FavesViewController: BaseVC {
         
         // I'm Here...
         isNavigationBarHidden = false
+        bindUI()
+        viewModel.attemptFetch()
     }
+    
+    // MARK: - Private Method
+    private func bindUI() {
+        viewModel.state.observe { [weak self] (state) in
+            guard let self = self else { return }
+            switch state {
+            case .success:
+                self.updateUI()
+            case .error(let err):
+                self.handleAlertView(title: nil, message: err ?? "")
+            default:
+                break
+            }
+        }.add(to: &disposal)
+    }
+    
+    private func updateUI() {
+        tableView.reloadData()
+        viewModel.setNSFetchedResultsControllerDelegate(self)
+        pageTitle = viewModel.title
+    }
+}
 
+// MARK: - Table Delegate Datasource
+
+extension FavesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.getNumberOfRow(section: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FaveTableViewCell.reuseIdentifier, for: indexPath) as! FaveTableViewCell
+        configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func configureCell(cell : FaveTableViewCell , indexPath: NSIndexPath) {
+        let item  = viewModel.getStationItemAtIndex(indexPath: indexPath as IndexPath)
+        cell.faveTableViewCellViewModel = FaveTableViewCellViewModel(model: item)
+    }
+}
+
+// MARK: - NSFetchedResultsControllerDelegate Method
+
+extension FavesViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch (type) {
+        case .insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+                tableView.backgroundView = nil
+                viewModel.attemptFetch()
+            }
+            break
+            
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break
+            
+        case.update:
+            if let indexPath = indexPath {
+                let cell = tableView.cellForRow(at: indexPath) as! FaveTableViewCell
+                configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+            }
+            break
+            
+        case .move:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+            
+        @unknown default:
+            fatalError()
+        }
+    }
 }
---
+++ d63181fffacf7f55df742aa964ab9a35/Context/FaveVC/View/FaveTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  FaveTableViewCell.swift
+//  d63181fffacf7f55df742aa964ab9a35
+//
+//  Created by amir on 27.12.2020.
+//  Copyright © 2020 Amir Daliri. All rights reserved.
+//
+
+import UIKit
+
+class FaveTableViewCell: BaseTableViewCell {
+
+    //MARK: - IBOutlets
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var eusLabel: UILabel!
+    @IBOutlet private weak var faveButton: UIButton!
+    
+    // MARK: - Properties
+    var faveTableViewCellViewModel: FaveTableViewCellViewModel = FaveTableViewCellViewModel() {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    
+    // MARK: - Private Functions
+    private func updateUI() {
+        nameLabel.text = faveTableViewCellViewModel.name
+        eusLabel.text = faveTableViewCellViewModel.eus
+    }
+    
+    @IBAction func faveButtonTapped(_ sender: Any) {
+    }
+}
